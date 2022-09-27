c7cfa89232f68dc662b39fbc4f020118738720ff
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-09-27 20:10:18 +0200

    añado src/ bien

---
+++ src/main/java/com/tiernoparla/repo/ejercicios/RepoEjercicios.java
@@ -0,0 +1,19 @@
+package com.tiernoparla.repo.ejercicios;
+
+public class RepoEjercicios {
+    public static void main(String[] args) {
+        
+        // printear numeros pares del 1 al 10
+        int n = 0;
+        System.out.println(n);
+        n = 1;
+        System.out.println(n);
+        //////////////////////
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);   
+            
+        } //for
+        
+        
+    } //main
+} //RepoEjercicios
